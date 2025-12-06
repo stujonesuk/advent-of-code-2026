378d3b0eec080a83e1e33c60e027647172f82cef
Stuart Jones <47328516+stujonesuk@users.noreply.github.com>
2025-12-06 20:42:36 +0000

    Day 6 Solution

--- aoc2026/day06/__init__.py
+++ aoc2026/day06/__init__.py
@@ -1,15 +1,35 @@
 """Day 06 solution."""
 
-from aoc2026.common import read_input, read_input_lines
+from aoc2026.common import read_input_lines
+from operator import add, mul
+from functools import reduce
 
+
+OPS = {
+    "+" : (add, 0),
+    "*" : (mul, 1)
+}
 
 def part_one():
     """Solve part one."""
     data = read_input_lines(6, 1)
     if data is None:
         return None
-    # TODO: Implement solution
-    return None
+
+    rows = list(map(lambda x: x.split(), data))
+
+    return sum(
+        (
+            reduce(
+                OPS[operator][0], 
+                (
+                    int(v[ix])
+                    for v in rows[:-1]
+                ), 
+                OPS[operator][1])
+            for ix, operator in enumerate(rows[-1])
+        )
+    )
 
 
 def part_two():
@@ -17,5 +37,18 @@ def part_two():
     data = read_input_lines(6, 2)
     if data is None:
         return None
-    # TODO: Implement solution
-    return None
+
+    total = 0
+    current_operator = None
+    current_operand = 0
+    for i in range(0, len(data[0])):
+        if not current_operator:
+            current_operator, current_operand = OPS[data[-1][i]]
+        if all(map(lambda r: r[i] == " ",data[:-1])):
+            current_operator = None
+            total += current_operand
+            current_operand = 0
+        else:
+            current_operand = current_operator(current_operand, int(reduce(lambda s, r: s + r[i], data[:-1], "")))
+    total += current_operand
+    return total
